aee633931ef449c1264096088b4d5574a30751f9
Michael Thingnes <thimic@gmail.com>
2019-05-14 15:01:14 +1200

    Updated launcher shebang line

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
     name='twicorder-search',
     packages=['twicorder', 'twicorder/queries'],
     scripts=['bin/twicorder', 'bin/twiread'],
-    version='0.2.3',
+    version='0.2.4',
     license='MIT',
     description='A Twitter crawler for Python 3 based on Twitter\'s public API',
     author='Michael Thingnes',
     author_email='thimic@gmail.com',
     url='https://github.com/thimic/twicorder-search',
     download_url=(
-        'https://github.com/thimic/twicorder-search/archive/v0.2.3.tar.gz'
+        'https://github.com/thimic/twicorder-search/archive/v0.2.4.tar.gz'
     ),
     keywords=['TWITTER', 'CRAWLER', 'RESEARCH'],
     install_requires=[
